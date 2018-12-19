29200bc8df2dc1420d57300b76bc9038de7c09c9
Victor <doctorhow23@googlemail.com>
2018-12-19 01:45:55 +0000

    start

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import './styles/PokeList.css'
+import PokeList from './PokeList';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <PokeList />
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/PokeCell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/PokeCell.css';
+
+const PokeCell = () => {
+    return(
+        <button className='poke-cell' />
+    )
+}
+
+
+export default PokeCell;
